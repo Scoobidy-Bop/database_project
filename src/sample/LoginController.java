cedc14e6c6ad1979503e418fa6a87f546547767c
Nate Kirsch <kirsch.nathaniel@gmail.com>
2020-11-27 21:55:52 -0800

    Begin connecting login screen to DB

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,27 +8,32 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class LoginController {
 
     public TextField first_name;
     public TextField last_name;
 
-    public void e_log_in() {
-        boolean ret = isName(first_name, last_name);
-        if (ret) {
-            String resource = "Search.fxml";
-            set_search_stage(resource);
-        } else {
-            PopUp.init_error("ERROR: Illegal name entered");
-        }
-    }
+    @FXML
+    private Button login_button;
 
-    public void log_in() {
+    public void log_in() throws Exception {
         boolean ret = isName(first_name, last_name);
         if (ret) {
+            String first = first_name.getText();
+            String last = last_name.getText();
+            boolean val = isEditor(first, last);
+            if (val) {
+                System.out.println(first + " " + last + " is an editor!");
+            } else {
+                System.out.println(first + " " + last + " is not an editor :(");
+            }
             String resource = "Search.fxml";
             set_search_stage(resource);
         } else {
@@ -47,13 +53,41 @@ public class LoginController {
     }
 
 
-    /*
-     * Function to check that t
-     */
     private boolean isName(TextField first, TextField last) {
         String first_name = first.getText();
         String last_name = last.getText();
         return first_name.matches("[a-zA-Z]+") && last_name.matches("[a-zA-Z]+");
+    }
+
+
+    private boolean isEditor(String first, String last) throws Exception{
+//        Properties prop = new Properties();
+//        FileInputStream in = new FileInputStream("./config.properties");
+//
+//        String dab = "nkirsch_DB";
+//        String host = prop.getProperty("host");
+//        String usr = prop.getProperty("user");
+//        String pwd = prop.getProperty("password");
+//        String url = "jdbc:mysql://" + host + "/" + dab;
+
+        String url = "jdbc:mysql://cps-database.gonzaga.edu/nkirsch_DB";
+        String usr = "nkirsch";
+        String pwd = "nkirsch21767533";
+
+        try (Connection cn = DriverManager.getConnection(url, usr, pwd)) {
+
+            String q = "SELECT * FROM editor WHERE first_name = ? AND last_name = ?";
+            PreparedStatement p_stmt = cn.prepareStatement(q);
+            p_stmt.setString(1, first);
+            p_stmt.setString(2, last);
+            ResultSet rs = p_stmt.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
